3ae55900ace2917d3743f3a5f771a46e18a37fef
Chris <41971210+legit228@users.noreply.github.com>
2018-09-13 18:30:40 -0500

    Add files via upload
    Updated navbar with proper home link

--- JS/Navbar.js
+++ JS/Navbar.js
@@ -1,5 +1,23 @@
 //Navbar for html
-document.write(`<div class="navbar"><a href="../index.html">Home</a><a href="https://github.com/ApolloHighSchoolKY/CodeCove">Github</a><a href="https://prod.teamgantt.com/gantt/schedule/?ids=1257545&public_keys=fHDTzH5DeW7d&zoom=d100&font_size=12&estimated_hours=0&assigned_resources=0&percent_complete=0&documents=0&comments=0&col_width=355&hide_header_tabs=0&menu_view=1&resource_filter=1&name_in_bar=0&name_next_to_bar=0&resource_names=1#user=&company=&custom=&date_filter=&hide_completed=false&color_filter=">GanttChart</a><a href="https://teamup.com/ks2y6w6whbvjz625sh">Calendar</a><a id="hacker" href="javascript:switchTheme();">Hacker Green</a><a href="../html/questions.html">Questions</a><a href="javascript:$('.navbar').hide();">Delete Navbar</a><a href="../html/contact.html" class="right">Contact Us</a></div>`);
+document.write(`<div class="navbar">
+  <div class="dropdown">
+    <button class="dropbtn">Contact Us
+      <i class="fa fa-caret-down"></i>
+    </button>
+    <div class="dropdown-content">
+      <p>Christopher.cox@stu.daviess.kyschools.us</p>
+      <p>Nathan.roberts@stu.daviess.kyschools.us</p>
+      <p>Jacob.jones3@stu.daviess.kyschools.us</p>
+    </div>
+  </div>
+  <a href="../index.html">Home</a>
+  <a href="https://github.com/ApolloHighSchoolKY/CodeCove">Github</a>
+  <a href="https://prod.teamgantt.com/gantt/schedule/?ids=1257545&public_keys=fHDTzH5DeW7d&zoom=d100&font_size=12&estimated_hours=0&assigned_resources=0&percent_complete=0&documents=0&comments=0&col_width=355&hide_header_tabs=0&menu_view=1&resource_filter=1&name_in_bar=0&name_next_to_bar=0&resource_names=1#user=&company=&custom=&date_filter=&hide_completed=false&color_filter=">GanttChart</a>
+  <a href="https://teamup.com/ks2y6w6whbvjz625sh">Calendar</a>
+  <a id="hacker" href="javascript:switchTheme();">Hacker Green</a>
+  <a href="html/questions.html">Questions</a>
+  <a href="javascript:$('.navbar').hide();">Delete Navbar</a>
+</div>`);
 
 //Method to switch theme
 var theme=1;
