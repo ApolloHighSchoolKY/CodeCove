d9be2350aa3c191dc59aebfb1d76b7c7c0d57cb1
NateDaddy <42280260+NateDaddy@users.noreply.github.com>
2018-10-05 10:35:43 -0500

    Add files via upload
    Works with functions in javascript and referencing them externally.

---
+++ js/ThemeChange.js
@@ -0,0 +1,17 @@
+function random_bg_color() {
+   var x = Math.floor(Math.random() * 256);
+   var y = Math.floor(Math.random() * 256);
+   var z = Math.floor(Math.random() * 256);
+   var xx = 255-x;
+   var yy = 255-y;
+   var zz = 255-z;
+   var bgColor = "rgb(" + x + "," + y + "," + z + ")";
+   document.body.style.backgroundColor = bgColor;
+var navBarColor = "rgb(" + xx + "," + yy + "," + zz + ")";
+  document.getElementById("navbar").style.backgroundColor = navBarColor;
+};
+function defaultTheme() {
+  document.getElementById("navbar").style.backgroundColor = "black";
+  document.getElementById("navbar").style.color = "white";
+  document.body.style.backgroundColor = "white";
+};
