4b18b142dc931a675b539072b889f5373aa8ae27
Chris Cox <chriscox228@gmail.com>
2019-01-07 11:22:37 -0600

    Updated all hyperlinks to new version for testing

--- js/altNavbar.js
+++ js/altNavbar.js
@@ -1,10 +1,9 @@
 document.write(`
   <div id="navbar" style="color: black; background-image: linear-gradient(to right, rgba(255,255,255,1), rgba(0,188,212,.7));" class="top">
    <div class="bar theme-d2 left-align">
-     <!--General Navbar-->
     <a class="bar-item button hide-medium hide-large right hover-white theme-d2" href="javascript:void(0);" onclick="openNav()"><i class="fa fa-bars"></i></a>
-    <a href="main.html" class="bar-item button cyan"><i class="fa fa-home margin-right"></i>Home</a>
-    <a href="lessons.html" class="bar-item button hide-small hover-cyan">Lessons Home</a>
+    <a href="/CodeCove/html/main.html" class="bar-item button cyan"><i class="fa fa-home margin-right"></i>Home</a>
+    <a href="/CodeCove/html/lessons.html" class="bar-item button hide-small hover-cyan">Lessons Home</a>
     <!--Theme Options-->
     <div class="dropdown-hover hide-small">
       <button class="button hover-cyan">Theme<i class="fa fa-caret-down"></i></button>
@@ -18,7 +17,7 @@ document.write(`
     <div class="dropdown-hover hide-small">
       <button class="button hover-cyan">Fun<i class="fa fa-caret-down"></i></button>
       <div class="dropdown-content card-4 bar-block">
-        <a href="games/ast.html" class="bar-item button hover-cyan">Asteroids</a>
+        <a href="/CodeCove/html/games/ast.html" class="bar-item button hover-cyan">Asteroids</a>
       </div>
     </div>
     <!--External Resources-->
@@ -41,18 +40,18 @@ document.write(`
 
    <!-- Navbar on small screens -->
    <div id="navDemo" class="bar-block theme-d2 hide hide-large hide-medium" style="color: black; background-color: transparent;">
-     <a href="main.html#team" class="bar-item button">Team</a>
-     <a href="main.html#lessons" class="bar-item button">Lessons</a>
-     <a href="main.html#accounts" class="bar-item button">Accounts</a>
-     <a href="main.html#contact" class="bar-item button">Contact</a>
+     <a href="/CodeCove/html/main.html#team" class="bar-item button">Team</a>
+     <a href="/CodeCove/html/main.html#lessons" class="bar-item button">Lessons</a>
+     <a href="/CodeCove/html/main.html#accounts" class="bar-item button">Accounts</a>
+     <a href="/CodeCove/html/main.html#contact" class="bar-item button">Contact</a>
      <a href="javascript:random_bg_color()" class = "bar-item button hover-cyan" >Random Theme</a>
      <a href="javascript:defaultTheme()" class = "bar-item button hover-cyan" >Default Theme</a>
      <a href="javascript:setRemoteTheme()" class = "bar-item button hover-cyan" >Save Theme</a>
-     <a href="games/ast.html" class="bar-item button hover-cyan">Asteroids (WIP)</a>
+     <a href="/CodeCove/html/games/ast.html" class="bar-item button hover-cyan">Asteroids (WIP)</a>
      <a href="https://github.com/ApolloHighSchoolKY/CodeCove" target="_blank" class="bar-item button hover-cyan">GitHub</a>
      <a href="https://prod.teamgantt.com/gantt/schedule/?ids=1257545&public_keys=fHDTzH5DeW7d&zoom=d100&font_size=12&estimated_hours=0&assigned_resources=0&percent_complete=0&documents=0&comments=0&col_width=355&hide_header_tabs=0&menu_view=1&resource_filter=1&name_in_bar=0&name_next_to_bar=0&resource_names=1#user=&company=&custom=&date_filter=&hide_completed=false&color_filter=" target="_blank" class="bar-item button hover-cyan">TeamGantt</a>
      <a href="https://teamup.com/ks2y6w6whbvjz625sh" target="_blank" class="bar-item button hover-cyan">Teamup Calendar</a>
-     <a href="login.html" class="bar-item button">Log In</a>
+     <!--<a href="login.html" class="bar-item button">Log In</a>-->
    </div>
    </div>
 `);
--- js/mainNavbar.js
+++ js/mainNavbar.js
@@ -47,11 +47,11 @@ document.write(`
       <a href="javascript:random_bg_color()" class = "bar-item button hover-cyan" >Random Theme</a>
       <a href="javascript:defaultTheme()" class = "bar-item button hover-cyan" >Default Theme</a>
       <a href="javascript:setRemoteTheme()" class = "bar-item button hover-cyan" >Save Theme</a>
-      <a href="games/ast.html" class="bar-item button hover-cyan">Asteroids (WIP)</a>
+      <a href="/CodeCove/html/games/ast.html" class="bar-item button hover-cyan">Asteroids (WIP)</a>
       <a href="https://github.com/ApolloHighSchoolKY/CodeCove" target="_blank" class="bar-item button hover-cyan">GitHub</a>
       <a href="https://prod.teamgantt.com/gantt/schedule/?ids=1257545&public_keys=fHDTzH5DeW7d&zoom=d100&font_size=12&estimated_hours=0&assigned_resources=0&percent_complete=0&documents=0&comments=0&col_width=355&hide_header_tabs=0&menu_view=1&resource_filter=1&name_in_bar=0&name_next_to_bar=0&resource_names=1#user=&company=&custom=&date_filter=&hide_completed=false&color_filter=" target="_blank" class="bar-item button hover-cyan">TeamGantt</a>
       <a href="https://teamup.com/ks2y6w6whbvjz625sh" target="_blank" class="bar-item button hover-cyan">Teamup Calendar</a>
-      <a href="login.html" class="bar-item button">Log In</a>
+      <!--<a href="login.html" class="bar-item button">Log In</a>-->
     </div>
   </div>
 `);
