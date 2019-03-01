083ad125b52f6ec30b710137b6a322d98f20dda9
NateDaddy <42280260+NateDaddy@users.noreply.github.com>
2019-03-01 12:51:06 -0600

    Add files via upload

---
+++ js/lessonsSidebar.js
@@ -0,0 +1,27 @@
+document.write(`<div class='customSB'>
+  <div class='sidebarElmnt'>
+    <div class='sidebarBreak'>
+        <img class='sidebarBreakImg' style='z-index:-1;margin-top:10%;height:80%;'src='/CodeCove/resources/syntaxFull.png'> </img>
+    </div>
+    <p id=syntax class='sidebarText'>Syntax</p>
+  </div>
+  <div id=org class='sidebarElmnt'>
+    <div class='sidebarBreak'>
+      <img style='margin-top:10%;height:80%;'src='/CodeCove/resources/orgFull.png'> </img>
+    </div>
+    <p class='sidebarText'>Elements</p>
+  </div>
+  <div id=comLangs class='sidebarElmnt'>
+    <div class='sidebarBreak'>
+        <img style='margin-top:10%;height:80%;'src='/CodeCove/resources/compatFull.png'> </img>
+    </div>
+    <div class='sidebarText'>Compatible Languages</div>
+  </div>
+  <div id=genBut class='sidebarElmnt'>
+    <div class='sidebarBreak'>
+      <img style='margin-top:10%;height:80%;'src='/CodeCove/resources/genFull.png'> </img>
+    </div>
+    <p href='#genRules' class='sidebarText'> <a href='#genRules'>General Rules </a> </p>
+  </div>
+  <div class='bottom center pNote' style='color:lightgray;position:absolute;bottom:50px;'> &copy; 2019 Team 4 Studios Productions, LLC</div>
+</div>`);
