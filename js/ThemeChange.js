32d77709992db46ebb9f63fb5a2e49b872a7ae4c
Chris Cox <chriscox228@gmail.com>
2019-03-05 11:55:56 -0600

    Theme Update

--- js/ThemeChange.js
+++ js/ThemeChange.js
@@ -11,8 +11,46 @@ function random_bg_color() {
    document.getElementById("navbar").style.backgroundImage = "linear-gradient(to right, rgba(255,255,255,1), rgba("+xx+","+yy+","+zz+",.7))";
 }
 function defaultTheme() {
-  document.getElementById("navbar").style.backgroundImage = "linear-gradient(to right, rgba(255,255,255,1), rgba(0,188,212,.7))";
-  document.getElementById("navbar").style.color = "black";
-  document.body.style.backgroundColor = "white";
-  console.log("Theme reset");
+  var rgb = textToRgb(getComputedStyle(document.body).getPropertyValue("--main-accent-color")).replace(/[^\d,]/g, '').split(',');
+  var rgbStr = rgb[0] + ","+rgb[1] + "," + rgb[2];
+  document.getElementById("navbar").style.backgroundImage = "linear-gradient(to right, "+ textToRgb(getComputedStyle(document.body).getPropertyValue("--main-bg-color")) +", "+ "rgba("+rgbStr+","+getComputedStyle(document.body).getPropertyValue("--nav-opacity")+")";
+  document.getElementById("navbar").style.color = "var(--main-text-color)";
+  document.getElementById("navHome").style.backgroundColor = getComputedStyle(document.body).getPropertyValue("--main-accent-color");
+  document.getElementById("navHome").style.color = getComputedStyle(document.body).getPropertyValue("--main-text-color");
+  document.body.style.backgroundColor = "var(--main-bg-color)";
+  
 }
+
+function hexToRgb(hex) {
+    // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
+    var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+    hex = hex.replace(shorthandRegex, function(m, r, g, b) {
+        return r + r + g + g + b + b;
+    });
+
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16)
+    } : null;
+}
+
+function rgbToHex(r, g, b) {
+    return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+}
+
+function textToRgb(englishColor) {
+    // Create a temporary div.
+    var div = $('<div></div>').appendTo("body").css('background-color', englishColor);
+    var computedStyle = window.getComputedStyle(div[0]);
+
+    // Get computed color.
+    var computedColor = computedStyle.backgroundColor;
+
+    // Cleanup temporary div.
+    div.remove();
+
+    // Done.
+    return computedColor;
+};
--- js/altNavbar.js
+++ js/altNavbar.js
@@ -1,8 +1,8 @@
 document.write(`
-  <div id="navbar" style="color: black; background-image: linear-gradient(to right, rgba(255,255,255,1), rgba(0,188,212,.7));" class="top">
+  <div id="navbar" class="top">
    <div class="bar theme-d2 left-align">
     <a class="bar-item button hide-medium hide-large right hover-white theme-d2" href="javascript:void(0);" onclick="openNav()"><i class="fa fa-bars"></i></a>
-    <a href="/CodeCove/index.html" class="bar-item button cyan"><i class="fa fa-home margin-right"></i>Home</a>
+    <a href="/CodeCove/index.html" class="bar-item button" id="navHome"><i class="fa fa-home margin-right"></i>Home</a>
     <a href="/CodeCove/html/lessons.html" class="bar-item button hide-small hover-cyan">Lessons Home</a>
     <!--Theme Options-->
     <div class="dropdown-hover hide-small">
@@ -15,7 +15,7 @@ document.write(`
     </div>
     <!--Fun Section-->
     <div class="dropdown-hover hide-small">
-      <button class="button hover-cyan">Fun<i class="fa fa-caret-down"></i></button>
+      <button class="button hover-cyan">Games<i class="fa fa-caret-down"></i></button>
       <div class="dropdown-content card-4 bar-block">
         <a href="/CodeCove/html/games/ast.html" class="bar-item button hover-cyan">Asteroids</a>
         <a href="/CodeCove/html/games/snake.html" class="bar-item button hover-cyan">Snake</a>
@@ -25,7 +25,7 @@ document.write(`
     <div class="dropdown-hover hide-small">
       <button class="button hover-cyan">Resources<i class="fa fa-caret-down"></i></button>
       <div class="dropdown-content card-4 bar-block">
-        <a href="https://github.com/ApolloHighSchoolKY/CodeCove" target="_blank" class="bar-item button hover-cyan">GitHub</a>
+        <a href="https://github.com/ApolloHighSchoolKY/CodeCove" target="_blank" class="bar-item button hover-cyan">Source Code</a>
         <a href="https://prod.teamgantt.com/gantt/schedule/?ids=1257545&public_keys=fHDTzH5DeW7d&zoom=d100&font_size=12&estimated_hours=0&assigned_resources=0&percent_complete=0&documents=0&comments=0&col_width=355&hide_header_tabs=0&menu_view=1&resource_filter=1&name_in_bar=0&name_next_to_bar=0&resource_names=1#user=&company=&custom=&date_filter=&hide_completed=false&color_filter=" target="_blank" class="bar-item button hover-cyan">TeamGantt</a>
         <a href="https://teamup.com/ks2y6w6whbvjz625sh" target="_blank" class="bar-item button hover-cyan">Teamup Calendar</a>
       </div>
